f5efc82d6e1f89da95032a60f18171ea90c7c6cc
Cortlin Sawyer <cortlin@Cortlins-MBP.lan>
2022-07-10 17:32:49 -0700

    Added functions to connect to the twitch IRC service

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 /*
-	1.[ ] Connect to the twitch IRC server via SSL connection
-	2.[ ] Send the PASS and NICK to authentica the bot user
+	1.[-] Connect to the twitch IRC server via SSL connection
+	2.[x] Send the PASS and NICK to authentica the bot user
 	3.[ ] Join a specific chat room, in this case okayotter
 	4.[ ] once connected, read the chat and log the results to the terminal
 	5.[ ] Parse the results for commands so we can send a response to a specific command
@@ -15,30 +15,67 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net"
 	"net/http"
+	"time"
 )
 
 //Object to store the ChuckNorris Joke
 type ChuckNorris struct {
     Value string `json:"value"`
-	err string
+}
+
+type chatConnection struct {
+    connection net.Conn
 }
 
 func main() {
-	joke := getJoke()
-	fmt.Println(joke)
+	//channel := "okayotter"
+    username := "okayotterbot"
+    //The auth key ideally should be stored in an env or config file
+    oauth := "oauth:Removed before pushing"
+	connectToTwitch(username, oauth)
+	fmt.Println(getJoke().Value)
 }
 
-//call the Chuck Norris API and return a joke as a string
-func getJoke() string {
+//call the Chuck Norris API and save to chuck1 object/struct
+func getJoke() *ChuckNorris {
     response, err := http.Get("https://api.chucknorris.io/jokes/random")
     if err != nil {
-        fmt.Println("Something went wrong: unable to retrieve a joke")
-        return ("Oops, something went wrong. Could not retrieve joke")
+        log.Fatal("Something went wrong: unable to retrieve a joke")
     }
 	//TODO: Probably don't ignore the error
     responseBody, _ := ioutil.ReadAll(response.Body)
-    chuck1 := ChuckNorris{}
+    chuck1 := &ChuckNorris{}
     json.Unmarshal([]byte(responseBody), &chuck1)
-    return chuck1.Value
+    return chuck1
+}
+
+//Connect to the twitch IRC service before connecting to a channel
+func connectToTwitch(username, oauth string) *chatConnection {
+    //connecting over non-SSL for now
+    response, err := net.Dial("tcp", "irc.chat.twitch.tv:6667")
+    if err != nil {
+        log.Fatal("cannot connect to twitch irc server", err)
+    }
+
+    //store the connection data into an object
+    irc := &chatConnection{
+        connection: response,
+    }
+
+	//must send in this order according to twitch
+    irc.send("PASS " + oauth)
+    irc.send("NICK " + username)
+    fmt.Printf("[%s] connected to twitch irc service\n", time.Now().Format(time.UnixDate))
+    return irc
+}
+
+//Send a message to the twitch server
+func (irc *chatConnection) send(msg string) {
+    _, err := irc.connection.Write([]byte(msg + "\r\n"))
+    if err != nil {
+        log.Fatal("disconnected from twitch irc server send", err)
+    }
 }
